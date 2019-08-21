2bc1c1f5f9b4c902a910118e9d813e4721e5a997
Nolan Emirot <nolane@zillowgroup.com>
2019-08-20 20:08:51 -0700

    Add one row to tree

---
+++ add_one_row_to_tree.py
@@ -0,0 +1,34 @@
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+class Solution:
+    def addOneRow(self, root: TreeNode, v: int, d: int) -> TreeNode:
+        queue = [[root, 1]]
+        process_queue = []
+    
+        if d == 1:
+            node = TreeNode(v)
+            node.left = root
+            return node
+        
+        while queue:
+            node, depth = queue.pop(0)
+            if depth == d - 1:
+                process_queue.append(node)
+            if node.right:
+                queue.append([node.right, depth+1])
+            if node.left:
+                queue.append([node.left, depth+1])
+        
+        while process_queue:
+            node = process_queue.pop(0)
+            left, right = node.left, node.right
+            node.left = TreeNode(v)
+            node.right = TreeNode(v)
+            node.left.left, node.right.right = left, right
+        
+        return root
